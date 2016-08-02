a255c5a204e62f8856c737c874783fe11fa0006f
Bright Antwi Boasiako <brightantwiboasiako@aol.com>
2016-08-02 09:25:54 +0000

    fixed a bug in LoanProtectionPolicy class

--- app/LoanProtectionPolicy.php
+++ app/LoanProtectionPolicy.php
@@ -109,10 +109,10 @@ class LoanProtectionPolicy extends Model implements Policy
      * @param array $borrowers
      * @return bool
      */
-    public function addloanBorrowers(array $borrowers){
+    public function addBorrowers(array $borrowers){
 
         // add all to borrowers and save
-        $this->setloanBorrowers($borrowers);
+        $this->setBorrowers($borrowers);
         return $this->save();
     }
 
@@ -167,7 +167,7 @@ class LoanProtectionPolicy extends Model implements Policy
         $policy->setInstitutionEmail($data['financier']['email']);
         $policy->setInstitutionPhone($data['financier']['phone']);
         $policy->setInstitutionBranch($data['financier']['branch']);
-        $policy->setloanBorrowers($data['borrowers']);
+        $policy->setBorrowers($data['borrowers']);
         $policy->setPolicyNumber($data['policy_number']);
 
         $policy->save();
@@ -195,7 +195,7 @@ class LoanProtectionPolicy extends Model implements Policy
         $this->institution_branch = $branch;
     }
 
-    private function setloanBorrowers(array $borrowers){
+    private function setBorrowers(array $borrowers){
 
         if($this->borrowers === null || empty(json_decode($this->borrowers, true))){
             $data = [];
